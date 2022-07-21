9e49463a6f37ba70ee9d00a56d98b9afe855f833
NANO <File changed>
2022-07-21 07:41:53 +0600

    3 submission

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -4,12 +4,8 @@ public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
     if(character == 'a' || character == 'e' || character == 'i'|| character == 'o' || character == 'u'){
         System.out.println("Vowel");
-    }else if(character >= 'b' || character <= 'd' && character >= 'f' || character <= 'h' &&
-            character >= 'j' || character <= 'n'
-            && character >= 'p' || character <= 't' && character >= 'v' || character <= 'z' ){
-        System.out.println("Consonant");
     }else{
-        System.out.println("wrong alphabet!");
+        System.out.println("Consonant");
     }
     }
 }
