c0c133e4385d663335e0d06dee731ebd5102060f
NANO <File changed>
2022-07-21 06:53:20 +0600

    1 submission

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        if(first > 0){
+            System.out.println("Aliquot");
+        }else{
+            System.out.println("Not aliquot");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
+        if(month == 12){
+            System.out.println("winter");
+        }else{
+            System.out.println("Wrong number");
+        }
 
     }
 }
