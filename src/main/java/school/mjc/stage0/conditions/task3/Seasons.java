a83ec0a82646ffccac4d4d3e886bf015a2a112fd
NANO <File changed>
2022-07-23 01:30:48 +0600

    10 submission

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,13 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month){
-        if(month == 6 || month <= 8) {
-            System.out.println("Summer");
-        }else if(month >= 3 || month <= 5){
+        if (month == 1 || month == 2 || month == 12) {
+            System.out.println("Winter");
+        } else if (month == 3 || month == 4 || month == 5) {
             System.out.println("Spring");
-        }else if(month == 1 || month == 12 || month == 2){
-                    System.out.println("Winter");
-        }else{
+        } else if (month == 6 || month == 7 || month == 8) {
+            System.out.println("Summer");
+
+        } else {
             System.out.println("wrong number!");
         }
     }
